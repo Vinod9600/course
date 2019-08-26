2db60a13a57be23bbd45666d5d875193f562e938
Vinod Durge <vinoddurge9600@gmail.com>
2019-08-26 22:00:03 +0530

    First Commit

---
+++ src/main/java/courseprojects/CourseMain.java
@@ -0,0 +1,14 @@
+package courseprojects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseMain.class, args);
+
+	}
+
+}
---
+++ src/main/java/courseprojects/topic/TopicService.java
@@ -0,0 +1,33 @@
+package courseprojects.topic;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+	private List<Topic> topics = new ArrayList<>(Arrays.asList(
+		new Topic("spring","Spring Framwork", "Spring framework Description"),
+		new Topic("Java","core java", "Core java Description"),
+		new Topic("Spring AOP","Spring AOP Framework", "Spring AOP Description")
+		));
+
+	public List<Topic> getAllTopics() {
+		return topics;
+	}
+	
+	public Topic getTopic(String id) {
+		Topic topic = topics.stream().filter(t->t.getId().equals(id)).findFirst().get();
+		if(topic == null) {
+			topic = topics.get(0);
+		}
+		return topic;
+	}
+
+	public void addTopic(Topic topic) {
+		// TODO Auto-generated method stub
+		topics.add(topic);
+	}
+}
